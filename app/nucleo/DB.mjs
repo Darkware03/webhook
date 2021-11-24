9622af36da5bf923ad1847013972f60ce69de244
rreyes <rsreyes@salud.gob.sv>
2021-11-24 16:27:16 -0600

    se esta trabajando en la parte de generar conecciones dinamicamente a la base de datos y poder tener multiples conecciones

--- app/nucleo/DB.mjs
+++ app/nucleo/DB.mjs
@@ -3,20 +3,10 @@ import db_config from "../../configs/db.mjs";
 
 export default class DB {
 
-    constructor() {
-        console.log('se ejecuta')
-        this.connect = null
-    }
-
-    conection(conection = null) {
-
-        DB.conection(conection)
-    }
-
     static conection(conection = null) {
-        if (conection) {
-            this.connect = conection
+        if(conection){
+            // console.log(db_config.conections.postgress)
+            console.log(db_config.conections[conection])
         }
-
     }
 }
