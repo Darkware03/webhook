0617374c64ec0d60b1af9bdfe5ce38c37afe4253
rreyes <rsreyes@salud.gob.sv>
2021-11-24 16:26:10 -0600

    se esta trabajando en el archivo principal de logica de la plantilla

--- configs/server.mjs
+++ configs/server.mjs
@@ -1,18 +1,38 @@
 import express from 'express'
+import NoAuthException from "../handlers/NoAuthException.mjs";
+import Handler from "../handlers/Handler.mjs";
+import DB from "../app/nucleo/DB.mjs";
 
-export default class Server{
-    constructor(){
-        this.app = express(); 
-        this.port = process.env.PORT || 8080;
-        
+export default class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 8000;
+        this.routes()
+        this.middlewares()
+        DB.conection('postgres')
+        // throw new NoAuthException()
     }
 
     middlewares() {
-        
+        this.app.use(Handler.logErrorMiddleware)
+        this.app.use(Handler.handlerError)
     }
+    prueba(){
+        throw new NoAuthException('prueba', 400, 'bat request')
+    }
+    routes() {
+        this.app.post('/user', async (req, res, next) => {
+            try{
+                this.prueba()
+                return res.status(200).send("prueba")
+            }catch (error) {
+                next(error)
+            }
+        })
+        this.app.all('*', ()=>{
 
-    routes(){
-        
+        })
     }
 
     start() {
