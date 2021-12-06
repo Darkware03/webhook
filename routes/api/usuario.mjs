a69adec2f6234bf12140eb5187ca1e2cb987f246
roberto reyes <robertostanleyreyes@gmail.com>
2021-12-06 13:19:42 -0600

    se agregaron los middelwares para usuario de auth

--- routes/api/usuario.mjs
+++ routes/api/usuario.mjs
@@ -1,8 +1,8 @@
 import UsuarioController from "../../app/controllers/UsuarioController.mjs";
-import Auth from "../../app/middlewares/Auth.mjs";
-export const usuario=(api)=>{
-    api.route.get('/users', [Auth],UsuarioController.index)
-    api.route.post('/users', UsuarioController.store)
-    api.route.put('/users/:id', UsuarioController.update)
-    api.route.delete('/users/:id', UsuarioController.destroy)
+
+export const usuario = (api) => {
+    api.route.get('/users', [api.middelwares], UsuarioController.index)
+    api.route.post('/users', [api.middelwares], UsuarioController.store)
+    api.route.put('/users/:id', [api.middelwares], UsuarioController.update)
+    api.route.delete('/users/:id', [api.middelwares], UsuarioController.destroy)
 }
