6e35b4e7307b47909138b545a79278850498e38a
roberto reyes <robertostanleyreyes@gmail.com>
2021-12-16 12:04:52 -0600

    se agrego el prefix de v1 en las rutas de la api

--- routes/api.mjs
+++ routes/api.mjs
@@ -6,10 +6,11 @@ import {validate} from "express-jsonschema";
 import {loginSchema} from "../app/schemas/LoginSchema.mjs";
 import Call from "../app/utils/Call.mjs";
 
+
 const router = Router()
+router.post('/v1/login', [validate({body: loginSchema})], Call(LoginController.login))
 
-router.post('/login', [validate({body: loginSchema})], Call(LoginController.login))
+router.use('/v1/users', [auth], routesUsers)
 
-router.use('/users', [auth], routesUsers)
 
 export default router
