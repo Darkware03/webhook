69a1e86cdc16640a5f929b47bb9dcb3a2ee75e2d
Carlos David Ascencio <carlos.ascencioa@salud.gob.sv>
2021-12-17 08:56:23 -0600

    Se cambio de openapi.json a swagger.yml Se movieron los archivos .yaml o .yml a la carpeta docs-api
    Falta dividir el archivo swagger en multiples archivos

--- routes/swagger.mjs
+++ routes/swagger.mjs
@@ -1,15 +1,21 @@
 //Aquí se hace el routing hacia la documentación de la API.
 import { Router } from "express";
 import swaggerUiExpress from "swagger-ui-express";
-
+import YAML from "yamljs";
 import {createRequire} from 'module';
-
+import path from "path";
+import {dirname} from 'path';
+import { fileURLToPath } from "url";
+//Creando variable encargada de la direccion del archivo YAML principal
 const require = createRequire(import.meta.url);
-const swaggerJson = require("../openapi.json");
-
+const __dirname = dirname(fileURLToPath(import.meta.url));
+//Se lee el archivo openapi.YAML
+const swaggerDocument = YAML.load(path.join(__dirname,'../docs-api/swagger.yml'));
 const router = Router();
+
+//Se establece la ruta hacia el Swagger usando el archivo openapi.yaml
 router.use('/api-docs',swaggerUiExpress.serve);
-router.get('/api-docs',swaggerUiExpress.setup(swaggerJson))
+router.get('/api-docs',swaggerUiExpress.setup(swaggerDocument))
 
 
 export default router
