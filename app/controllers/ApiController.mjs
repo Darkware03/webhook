fdbed3f059cadd7e664a0becfea1b87b3cd01b25
roberto reyes <robertostanleyreyes@gmail.com>
2021-12-20 10:03:05 -0600

    trabajando en el refresh_token

--- app/controllers/ApiController.mjs
+++ app/controllers/ApiController.mjs
@@ -2,10 +2,10 @@ import Usuario from "../models/Usuario.mjs";
 import HttpCode from "../../configs/httpCode.mjs";
 import bcrypt from 'bcryptjs'
 import NoAuthException from "../../handlers/NoAuthException.mjs";
-import GenerarJwt from "../utils/GenerarJwt.mjs";
+import Auth from "../utils/Auth.mjs";
 import moment from "moment-timezone";
 
-export default class LoginController {
+export default class ApiController {
     static async login(req, res) {
         const {email, password} = req.body
 
@@ -23,15 +23,18 @@ export default class LoginController {
             throw new NoAuthException('UNAUTHORIZED', HttpCode.HTTP_UNAUTHORIZED, 'Credenciales no validas')
         }
 
-        await usuario.update({last_login: moment().tz('America/El_Salvador').format()})
+    await usuario.update({last_login: moment().tz('America/El_Salvador').format()})
 
-        const token = await GenerarJwt.create({
+        const token = await Auth.createToken({
             id: usuario.id,
             email: usuario.email,
         })
+        const refresh_token = await Auth.refresh_token(usuario)
 
         return res.status(HttpCode.HTTP_OK).json({
-            token
+            token,
+            refresh_token,
+            user: usuario
         })
 
     }
---
+++ app/models/RefreshToken.mjs
@@ -0,0 +1,36 @@
+import DB from "../nucleo/DB.mjs";
+import psql from "sequelize";
+import Usuario from "./Usuario.mjs";
+
+class RefreshToken extends psql.Model {
+}
+
+
+RefreshToken.init({
+    id: {
+        type: psql.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    refresh_token: {
+        type: psql.Sequelize.STRING,
+    },
+    id_usuario: {
+        type: psql.Sequelize.INTEGER
+    },
+    valid: {
+        type: psql.Sequelize.DATE
+    }
+}, {
+    timestamps: false,
+    tableName: 'refresh_tokens',
+    sequelize: DB.connection(),
+})
+
+await RefreshToken.belongsTo(Usuario, {
+    foreignKey: 'id_usuario'
+})
+
+await RefreshToken.sync()
+
+export default RefreshToken
