1f496e5c5510a740940ea92d3f564e1944a8235d
Everth Giovanni Barrientos Guzman <ebarrientos@salud.gob.sv>
2022-01-05 10:48:43 -0600

    creacion de refreshToken

--- app/controllers/ApiController.mjs
+++ app/controllers/ApiController.mjs
@@ -1,10 +1,11 @@
-import {Usuario} from "../models/index.mjs";
+import {Usuario, Perfil, Rol} from "../models/index.mjs";
 import HttpCode from "../../configs/httpCode.mjs";
 import bcrypt from 'bcryptjs'
 import NoAuthException from "../../handlers/NoAuthException.mjs";
 import Auth from "../utils/Auth.mjs";
 import moment from "moment-timezone";
 import {RefreshToken} from "../models/index.mjs";
+import BaseError from "../../handlers/BaseError.mjs";
 
 
 export default class ApiController {
@@ -42,19 +43,37 @@ export default class ApiController {
     }
 
     static async RefreshToken(req, res) {
-        /* const token_valid = await RefreshToken.findOne({where: {refresh_token: req.body.refresh_token}})
-
-         console.log(token_valid.valid.getTime() < new Date().getTime())
-         if (!token_valid || token_valid.valid.getTime() < new Date().getTime()) throw new NoAuthException()
-
-         return res.status(HttpCode.HTTP_OK).json({
-
-         })*/
-
-        const tokens = await RefreshToken.findAll({
-            include: Usuario
+        let roles = [];
+        const refreshTokenExist = await RefreshToken.findOne({
+            where: {
+                refresh_token:req.body.refresh_token
+            },
+            include: [{model:Usuario,include: [Rol, {model:Perfil, include: [Rol]}]}]
+        })
+        for (let i = 0; i < refreshTokenExist.Usuario.Perfils.length; i++){
+            for (let e = 0; e < refreshTokenExist.Usuario.Perfils[i].Rols.length; e++){
+                roles.push(refreshTokenExist.Usuario.Perfils[i].Rols[e].name)
+            }
+        }
+        for (let i = 0; i < refreshTokenExist.Usuario.Rols.length; i++){
+           if (! (roles.find(rolename => rolename === refreshTokenExist.Usuario.Rols[i].name))){
+               roles.push(refreshTokenExist.Usuario.Rols[i].name)
+           }
+        }
+        if (! refreshTokenExist) throw new BaseError('NOT_FOUND',HttpCode.HTTP_BAD_REQUEST,'Error al realizar la peticion...')
+        const tokenValidTime =new Date(moment(refreshTokenExist.valid).format()).getTime();
+        const nowTime = new Date(moment().tz('America/El_Salvador').format()).getTime();
+        if (tokenValidTime < nowTime) throw new NoAuthException('UNAUTHORIZED', HttpCode.HTTP_UNAUTHORIZED, 'El refresh token porporcionado no es valido')
+        const token = await Auth.createToken({
+            email: refreshTokenExist.Usuario.email,
+            roles: roles,
         })
 
-        return res.status(HttpCode.HTTP_OK).json(tokens)
+        const newRefreshToken = await Auth.refresh_token(refreshTokenExist.Usuario)
+        return res.status(HttpCode.HTTP_OK).json({
+            token,
+            refresh_token: newRefreshToken,
+            user: refreshTokenExist.Usuario
+        })
     }
 }
