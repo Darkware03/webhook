9f9521ad26468274fbd76660801d44a53e7d8a2b
Bryan Josué Aguilar Román <bryan.aguilar@salud.gob.sv>
2022-01-13 15:34:53 -0600

    Actualizacion de email y notificacion por correo
    Modificacion del email del usuario loggeado(Se requiere el email en el JSON de entrada *validacion con UsuarioUpdateEmailSchema*) y confirmacion por correo

--- routes/api/usuario.mjs
+++ routes/api/usuario.mjs
@@ -2,6 +2,7 @@ import { validate } from 'express-jsonschema';
 import { Router } from 'express';
 import UsuarioController from '../../app/controllers/UsuarioController.mjs';
 import usuarioCreateSchema from '../../app/schemas/UsuarioCreateSchema.mjs';
+import usuarioUpdateEmailSchema from '../../app/schemas/UsuarioUpdateEmailSchema.mjs';
 import Call from '../../app/utils/Call.mjs';
 
 const router = Router();
@@ -11,6 +12,7 @@ router.delete('/:id_usuario/perfiles', Call(UsuarioController.destroyUserPerfil)
 router.post('/:id_usuario/roles', Call(UsuarioController.addUserRole));
 router.delete('/:id_usuario/roles', Call(UsuarioController.destroyUserRol));
 router.put('/:id', Call(UsuarioController.update));
+router.put('/update/mail', [validate({ body: usuarioUpdateEmailSchema })], Call(UsuarioController.updateEmail));
 router.delete('/:id', Call(UsuarioController.destroy));
 router.post('/', [validate({ body: usuarioCreateSchema })], Call(UsuarioController.store));
 router.get('/:id', Call(UsuarioController.show));
