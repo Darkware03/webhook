5887170dcd66cb9f6089ed0130ec59292de7a61f
roberto.luna <roberto.luna@salud.gob.sv>
2022-01-19 11:26:13 -0600

    correccion de errores

--- app/middlewares/Auth.mjs
+++ app/middlewares/Auth.mjs
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import NoAuthException from '../../handlers/NoAuthException.mjs';
 import { Usuario } from '../models/index.mjs';
 import HttpCode from '../../configs/httpCode.mjs';
+import moment from 'moment';
 
 // eslint-disable-next-line consistent-return
 const Auth = async (req, res, next) => {
@@ -15,14 +16,14 @@ const Auth = async (req, res, next) => {
     const token = authorization[1];
     const { id } = jwt.verify(token, process.env.SECRET_KEY);
     const decoded = jwt.decode(token, process.env.SECRET_KEY);
-    const fechaCreacionToken = new Date(decoded.iat*1000);
+    const fechaCreacionToken = new Date(moment(decoded.iat*1000).format()).getTime();
 
     const usuario = await Usuario.findOne({
       where: { id, is_suspended: false },
     });
 
-    const fechaValidacionToken = new Date(usuario.token_valid_after);
-
+    const fechaValidacionToken = new Date(moment(usuario.token_valid_after).format()).getTime();
+    
     if (usuario.is_suspended) throw new NoAuthException();
 
      if(fechaValidacionToken >= fechaCreacionToken ){
