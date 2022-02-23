3e87d526387608bc617db92c69a67640cc4b3654
Bryan Josué Aguilar Román <bryan.aguilar@salud.gob.sv>
2022-02-23 13:43:28 -0600

    UPDATE de Seeder
    Los roles de PERFIL, ROL, RUTA, USUARIO

--- routes/api/ruta.mjs
+++ routes/api/ruta.mjs
@@ -11,9 +11,9 @@ const router = Router();
 router.get('/', Call(RutaController.index));
 
 // router.post('/', [validate({body: usuarioCreateSchema})], Call(UsuarioController.store))
-router.post('/', [validateRole('ROLE_RUTA_LIST'), validate(rutaCreateSchema)], Call(RutaController.store));
-router.get('/get-rutas', [validateRole('ROLE_RUTA_VIEW')], Call(RutaController.getRutas));
-router.get('/:id', [validateRole('ROLE_RUTA_VIEW')], Call(RutaController.show));
+router.post('/', [validateRole('ROLE_RUTA_CREATE'), validate(rutaCreateSchema)], Call(RutaController.store));
+router.get('/get-rutas', [validateRole('ROLE_RUTA_LIST')], Call(RutaController.getRutas));
+router.get('/:id', [validateRole('ROLE_RUTA_LIST')], Call(RutaController.show));
 router.post('/:id_ruta/roles', [validateRole('ROLE_RUTA_ROL_CREATE'), validate(usuarioAddUserRoleSchema)], Call(RutaController.addRutaRole));
 router.put('/:id', [validateRole('ROLE_RUTA_UPDATE'), validate(rutaCreateSchema)], Call(RutaController.update));
 router.delete('/:id', validateRole('ROLE_RUTA_DELETE'), Call(RutaController.destroy));
--- routes/api/usuario.mjs
+++ routes/api/usuario.mjs
@@ -11,7 +11,7 @@ import validateRole from '../../app/middlewares/validateRole.mjs';
 
 const router = Router();
 router.get('/metodos-autenticacion-usuario', [validateRole('ROLE_USER_METHOD_LIST')], Call(UsuarioController.getMetodosUsuario));
-router.get('/', [validateRole('ROLE_USER_VIEW')], Call(UsuarioController.index));
+router.get('/', [validateRole('ROLE_USER_LIST')], Call(UsuarioController.index));
 router.post('/:id_usuario/perfiles', [validateRole('ROLE_USER_PERFIL_CREATE'), validate(usuarioAddUserProfileSchema)], Call(UsuarioController.addUserProfile));
 router.delete('/:id_usuario/perfiles', [validateRole('ROLE_USER_PERFIL_DELETE')], Call(UsuarioController.destroyUserPerfil));
 router.post('/:id_usuario/roles', [validate(usuarioAddUserRoleSchema)], Call(UsuarioController.addUserRole));
@@ -19,7 +19,7 @@ router.delete('/:id_usuario/roles', [validateRole('ROLE_USER_ROL_DELETE')], Call
 router.put('/:id', [validateRole('ROLE_USER_UPDATE')], Call(UsuarioController.update));
 router.delete('/:id', [validateRole('ROLE_USER_DELETE')], Call(UsuarioController.destroy));
 router.post('/', [validateRole('ROLE_USER_CREATE'), validate(usuarioCreateSchema)], Call(UsuarioController.store));
-router.get('/:id', [validateRole('ROLE_USER_PERFIL_VIEW')], Call(UsuarioController.show));
+router.get('/:id', [validateRole('ROLE_USER_PERFIL_LIST')], Call(UsuarioController.show));
 router.put('/update/password', [validateRole('ROLE_USER_PASSWORD_UPDATE'), validate(usuarioPasswordUpdate)], Call(UsuarioController.updatePassword));
 router.put('/update/email', [validateRole('ROLE_USER_EMAIL_DELETE'), validate(usuarioUpdateEmailSchema)], Call(UsuarioController.updateEmail));
 router.post('/2fa/add', Call(UsuarioController.storeMethodUser));
