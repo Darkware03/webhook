ebcb3d9ceb647f471eda13d733df40b0372ddfd4
Francisco <frank.01.1998.fm@gmail.com>
2022-04-27 15:23:11 -0600

    se agrego unitTest para perfiles

---
+++ tests/Perfil.spec.mjs
@@ -0,0 +1,37 @@
+import { it, describe } from 'mocha';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import url from './config.mjs';
+
+chai.use(chaiHttp);
+
+describe('Test de post de usuarios', () => {
+  let token;
+  beforeEach((done) => {
+    // se ejecuta antes de cada prueba en este bloque
+    chai.request(url)
+      .post('/api/v1/login')
+      .send({
+        email: 'admin@salud.gob.sv',
+        password: 'admin',
+      })
+      .end((err, res) => {
+        token = res.body.token;
+
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+  it('Test de get perfiles [get] /api/v1/perfiles, caso exitoso', (done) => {
+    chai.request(url)
+      .get('/api/v1/perfiles')
+      .set('Authorization', `Bearer ${token}`)
+      .then((response) => {
+        expect(response).to.have.status(200);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
