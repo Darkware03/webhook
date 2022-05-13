c29d3422289a01866d254bf43999966081e620e5
roberto.luna <roberto.luna@salud.gob.sv>
2022-05-13 10:59:46 -0600

    creando esquema de update usuario

--- routes/api/usuario.mjs
+++ routes/api/usuario.mjs
@@ -6,13 +6,14 @@ import Call from '../../app/utils/Call.mjs';
 import usuarioPasswordUpdate from '../../app/schemas/UsuarioPasswordUpdateSchema.mjs';
 import usuarioUpdateEmailSchema from '../../app/schemas/UsuarioUpdateEmailSchema.mjs';
 import validateRole from '../../app/middlewares/validateRole.mjs';
+import usuarioUpdateSchema from '../../app/schemas/UsuarioUpdateSchema.mjs';
 
 const router = Router();
 router.get('/metodos-autenticacion-usuario', [validateRole('ROLE_USER_METHOD_LIST')], Call(UsuarioController.getMetodosUsuario));
 router.get('/', [validateRole('ROLE_USER_LIST')], Call(UsuarioController.index));
 router.post('/', [validateRole('ROLE_USER_CREATE'), validate(usuarioCreateSchema)], Call(UsuarioController.store));
 router.get('/:id', [validateRole('ROLE_USER_LIST')], Call(UsuarioController.show));
-router.put('/:id', [validateRole('ROLE_USER_UPDATE')], Call(UsuarioController.update));
+router.put('/:id', [validateRole('ROLE_USER_UPDATE'), validate(usuarioUpdateSchema)], Call(UsuarioController.update));
 router.delete('/:id', [validateRole('ROLE_USER_DELETE')], Call(UsuarioController.destroy));
 router.put('/update/password', [validateRole('ROLE_USER_PASSWORD_UPDATE'), validate(usuarioPasswordUpdate)], Call(UsuarioController.updatePassword));
 router.put('/update/email', [validateRole('ROLE_USER_EMAIL_UPDATE'), validate(usuarioUpdateEmailSchema)], Call(UsuarioController.updateEmail));
