3cf86d8b9ce236755d0b7308d3e2c862238eecbc
roberto.luna <roberto.luna@salud.gob.sv>
2022-05-13 14:04:15 -0600

    modificacion de createSchema

--- app/schemas/RolCreateSchema.mjs
+++ app/schemas/RolCreateSchema.mjs
@@ -2,17 +2,21 @@ const rolCreateSchema = {
   $id: 'http://example.com/schemas/rolCreateSchema.json#',
   type: 'object',
   properties: {
+    idTipoRol: {
+      $ref: 'defs.json#/definitions/integer',
+    },
     name: {
       $ref: 'defs.json#/definitions/string',
     },
   },
-  required: ['name'],
+  required: ['idTipoRol', 'name'],
   errorMessage: {
     required: {
       name: 'El campo name es requerido',
     },
     properties: {
       name: 'El campo name debe ser de tipo alfanumérico',
+      idTipoRol: 'El campo idTipoRol debe ser de tipo numérico',
     },
   },
 };
