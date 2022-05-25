e6e95bd622dbd55669c3c35b22cfd46629814a3c
Rony Garcia <rony.garcia@salud.gob.sv>
2022-05-25 11:44:42 -0600

    Se convirtio a numero parametros de paginacion

--- app/controllers/RutaController.mjs
+++ app/controllers/RutaController.mjs
@@ -25,9 +25,9 @@ export default class RutaController {
       offset: perPage * (page - 1),
     });
     return res.status(HttpCode.HTTP_OK).json({
-      page,
+      page: Number(page),
       per_page: Number(perPage),
-      total_rows: totalRows,
+      total_rows: Number(totalRows),
       body: rutas,
     });
   }
