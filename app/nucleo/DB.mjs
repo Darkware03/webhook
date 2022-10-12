98b935ddf097fd817eeba2b3620ee42af9a6a13a
Rony Garcia <rony.garcia@salud.gob.sv>
2022-10-12 08:22:18 -0600

    fix: correccion de conexion a BD con modelos

--- app/nucleo/DB.mjs
+++ app/nucleo/DB.mjs
@@ -2,23 +2,29 @@ import { Sequelize } from 'sequelize';
 import dbConfig from '../../configs/db.mjs';
 
 export default class DB {
-  static connection(connection = null) {
-    const predefinida = dbConfig.default;
-    let config = {};
-    if (connection) config = dbConfig.connections[connection];
-    else config = dbConfig.connections[predefinida];
+  conn;
 
-    return new Sequelize(config.options.db_name, config.options.db_username, config.options.db_password, {
-      host: config.options.db_host,
-      port: config.options.db_port,
-      dialect: config.motor,
-      logging: process.env.DB_LOGGER === 'true',
-    });
+  static connection() {
+    const predefinida = dbConfig.default;
+    const config = dbConfig.connections[predefinida];
+
+    if (!this.conn) {
+      this.conn = new Sequelize(config.options.db_name, config.options.db_username, config.options.db_password, {
+        host: config.options.db_host,
+        port: config.options.db_port,
+        dialect: config.motor,
+        timezone: process.env.TIMEZONE || '-06:00',
+        logging(str) {
+          if (process.env.DB_LOGGER === 'true') console.log(str);
+        },
+      });
+    }
+    return this.conn;
   }
 
-  static async testing(connection = null) {
+  static async testing() {
     try {
-      await this.connection(connection).authenticate();
+      await this.connect().authenticate();
       return true;
     } catch (e) {
       return false;
