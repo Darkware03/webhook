bcb0211eed3dfb4dda17b57e1ffca3b9cff96959
ebarrientos <everth.barrientos@salud.gob.sv>
2023-03-14 13:53:19 -0600

    prueba

--- app/services/SINGBOX.mjs
+++ app/services/SINGBOX.mjs
@@ -24,7 +24,7 @@ export default class SINGBOX {
             const documentUrl = 'https://download.hightech-corp.com/fel/clientes-prueba/sample.pdf';
             const formData = new FormData();
             formData.append('url_in', documentUrl);
-            formData.append('url_out', `${process.env.HOST}:${process.env.PORT}/api/v1/listen`);
+            formData.append('url_out', `https://api-webhookfirma.egob.sv/api/v1/listen`);
            // formData.append('urlback', 'http://localhost:8005/api/v1/listen');
             formData.append('env', process.env.ENV_SING);
             formData.append('format', 'pades');
