451c66baa4c97bbd0f199a81aca247235d0e0106
ebarrientos <everth.barrientos@salud.gob.sv>
2023-11-26 17:39:02 -0600

    se modifica la creacion del socket para que se cree el canal en el mismo endpoint

--- configs/server.mjs
+++ configs/server.mjs
@@ -2,22 +2,20 @@ import express from 'express';
 import { createServer } from 'http';
 import cors from 'cors';
 import fileupload from 'express-fileupload';
-import corsConfig from './cors.mjs';
-import {Server as ServerIo }  from 'socket.io';
 
 class Server {
   constructor() {
     this.app = express();
     this.server = createServer(this.app);
     this.port = process.env.PORT || 8000;
-
     this.host = process.env.HOST || 'localhost';
     this.middlewares();
+    this.routes(); // Agregar la configuración de las rutas
+    this.start(); // Iniciar el servidor
   }
+
   middlewares() {
-    this.app.use(fileupload({
-      createParentPath: true,
-    }));
+    this.app.use(fileupload({ createParentPath: true }));
     this.app.use(cors());
     this.app.use(express.static('public'));
     this.app.use(express.json());
@@ -25,18 +23,17 @@ class Server {
   }
 
   routes() {
-    // Ruta de ejemplo: GET en '/saludo'
-    this.app.get('/servicelogs', (req, res) => {
-      console.log(req)
+    // Ruta de ejemplo: POST en '/servicelogs'
+    this.app.post('/servicelogs', (req, res) => {
+      console.log(req.body); // Muestra el cuerpo de la solicitud en la consola
       return res.status(200).json({ message: "SE ACCEDIO." });
     });
     // Puedes añadir más rutas aquí si es necesario
   }
-  start() {
 
+  start() {
     this.server.listen(this.port, this.host, () => {
-      // eslint-disable-next-line no-console
-      console.log(`http://${this.host}:${this.port}`);
+      console.log(`Servidor Express escuchando en http://${this.host}:${this.port}`);
     });
   }
 }
